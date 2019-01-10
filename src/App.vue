531f7b951546ad4a31a56712bf2b682d1e059c2c
Liu Ren <liuren@cc1st.com>
2019-01-10 12:39:49 +0800

    初始化框架

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,22 @@
 </template>
 
 <script>
+  import Cookies from 'js-cookie'
+
   export default {
     name: 'App',
+    beforeCreate() {
+      if(Cookies.get('name')){
+        this.$router.push('/home');
+      }else{
+        this.$router.push('/login');
+      }
+
+    }
   }
 </script>
 
 <style lang="less">
-
   #app {
 
   }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { Field } from 'vant';
 import router from './router'
 import '@babel/polyfill'
 
+Vue.use(Field)
 
 new Vue({
   el: '#app',
