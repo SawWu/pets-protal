a74d0e350afd1facdecba47531a0a8048c1b14b6
Liu Ren <liuren@cc1st.com>
2019-01-10 12:43:05 +0800

    初始化框架

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    Login
+    <van-cell-group>
+      <van-field
+          v-model="userName"
+          label="用户名"
+          placeholder="请输入用户名"
+          required
+      />
+
+      <van-field
+          v-model="password"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+      />
+    </van-cell-group>
   </div>
 </template>
 
@@ -8,8 +23,10 @@
 
   export default {
     name: 'Login',
-    components: {
-
-    },
+    components: {},
+    data: () => ({
+      userName: '',
+      password: ''
+    }),
   }
 </script>
